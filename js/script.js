67c3e47c746cbc42877544607a7e3d229b499adc
Emmanuela Joy <emmajoy81@gmail.com>
2021-03-02 10:59:42 +0300

    add responsive design

--- js/script.js
+++ js/script.js
@@ -13,33 +13,18 @@ $(document).ready(function () {
         $(".product-hidden").toggle();
         $(".product-showing").toggle();
     });
-    /*
-        $('.item').hover(function () {
-            $(this).stop().animate({
-                opacity: .4
-            }, 200);
-            $('#project-' + this.id).css('visibility', 'visible');
-        }, function () {
-            $(this).stop().animate({
-                opacity: 1
-            }, 500);
-            $('#project-' + this.id).stop().css('visibility', 'hidden');
-        }); */
 
-
-    $(".item").mouseover(function () {
+    $(".item").hover(function () {
         $(this).stop().animate({
             opacity: .4
         }, 200);
-        $(".project").css('visibility', 'visible');
-    });
-    $(".item").mouseout(function () {
+        $('.project').css('visibility', 'visible');
+    }, function () {
         $(this).stop().animate({
             opacity: 1
         }, 500);
-        $(".project").stop().css('visibility', 'hidden');
+        $('.project').css('visibility', 'hidden');
     });
-
 
     $("#submitBtn").click(function (event) {
         let name = $("#name").val();
